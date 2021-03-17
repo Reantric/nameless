042f6bb01ea81a1a429097116f953afeccc15d6e
John <44213272+protonlaser91@users.noreply.github.com>
2021-03-17 14:10:00 -0400

    made clean
    Added some basic colour support for differing tones of sentiment.

--- src/events/sentiment.ts
+++ src/events/sentiment.ts
@@ -3,6 +3,15 @@ import { IBotEvent } from "../api/eapi";
 var https = require('follow-redirects').https;
 var fs = require('fs');
 
+export let values: {[id: string]: number}  = {
+  "P+": 1,
+  "P": 0.5,
+  "NEU" : 0,
+  "NONE" : 0,
+  "N" : -0.5,
+  "N+": -1,
+}
+
 export default class sentiment implements IBotEvent {
 
 
@@ -18,7 +27,7 @@ export default class sentiment implements IBotEvent {
 
 
     async runEvent(msg: Discord.Message, Bot: Discord.Client): Promise<void> {
-        if (msg.channel.id != `821743564731973674`)
+        if (msg.channel.id != `821743564731973674` || msg.content.startsWith("!"))
                 return;
         
 
@@ -35,16 +44,43 @@ export default class sentiment implements IBotEvent {
               
                 var req = https.request(options, function (res: { on: (arg0: string, arg1: { (chunk: any): void; (chunk: any): void; (error: any): void; }) => void; }) {
                 var chunks: any[] = [];
-              
+                  
                   res.on("data", function (chunk: any) {
                     chunks.push(chunk);
                   });
               
                   res.on("end", function (chunk: any) {
-                    var body = Buffer.concat(chunks);
-                    let content = body.toString();
-                    console.log(content);
-                    msg.channel.send(content);
+                    var body: any = JSON.parse(Buffer.concat(chunks).toString());
+                    let score = body[`score_tag`]
+
+                    let colorScheme = () => {
+                      let j = () => {
+                        var newSize = 16777214 - 2;
+                        var oldSize = 1 - (-1);
+                        var oldScale: number = values[score] - (-1);
+                        return (newSize * oldScale / oldSize) + 2 as number;
+                    
+                    }
+                      return Math.floor(j()) as number 
+                    }
+
+                    const sentiment = new Discord.MessageEmbed()
+                                .setAuthor(msg.author.username,msg.author.avatarURL()!,``)
+                                .setColor(colorScheme())
+                                .addField('Sentiment',score,true)
+                                .addField('Subjectivity',body[`subjectivity`],true)
+                                .addField('Irony',body[`irony`],true)
+                                .addField('Agreement',body[`agreement`],true)
+                                .setFooter(`${body[`confidence`]}% confident`,Bot.user!.avatarURL()!)
+                                .setTimestamp(new Date());
+                msg.channel.send(sentiment);
+                  /*  while (content.length > 2000){
+                      msg.channel.send(content.slice(0,2000));
+                      content = content.slice(2000);
+                    }
+                 //   console.log(content);
+                    msg.channel.send(content);*/
+
                   });
               
                   res.on("error", function (error: any) {
