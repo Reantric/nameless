c960c46e6fb09fdc99add8addbc63fef09582402
fanman1 <59294394+fanman1@users.noreply.github.com>
2021-03-17 22:11:57 -0400

    added strikes to delete

--- src/events/delete1.ts
+++ src/events/delete1.ts
@@ -2,9 +2,9 @@ import * as Discord from "discord.js";
 import { IBotEvent } from "../api/eapi";
 var https = require('follow-redirects').https;
 var fs = require('fs');
-
+import * as db from "quick.db";
 //export let credits: number = NaN
-var forbiddenWords=['fach'];
+var forbiddenWords=['fuck','bitch','hell','niga','nigger','nigga','fucker','fuk','ginder','shit','cock','cok','negro','chink','shitter', 'sanjit'];
 export default class delete1 implements IBotEvent {
 
     private readonly _command = "delete1"
@@ -21,14 +21,19 @@ export default class delete1 implements IBotEvent {
     
 
     async runEvent(msg: Discord.Message, Bot: Discord.Client): Promise<void> {
-        if (msg.channel.id != `821743564731973674` || msg.content.startsWith("!"))
+        if (msg.channel.id != `816678642254872680` || msg.content.startsWith("!"))
                 return;
       
         for(var i=0;i<forbiddenWords.length;i++){
           if (msg.content.includes(forbiddenWords[i])) {
+            //const mentionedUser = msg.mentions.users.first();
+            let strikeAmount = db.add(`${msg.author.id}.strikes`,1);
+            msg.reply(`${msg.author.username} now has ${db.get(`${msg.author.id}.strikes`)} strikes!`)
+            console.log(strikeAmount)
             msg.delete();
             console.log('Deleted message due to forbidden word');
-            msg.author.send("Hey, you used a bad word in your recent message. It was deleted and you were striked.")
+            msg.author.send("Hey, you used a bad word in your recent message. It was deleted and you were given a strike."
+            +"You now have"+`${db.get(`${msg.author.id}.strikes`)} strikes!`);
             // delete message, log, etc.
             break;
           }
